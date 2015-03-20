6b246d19810f9f41c76014cf588de690f8fd677f
Marc Ducobu <marc@champs-libres.coop>
2015-03-20 18:09:09 +0100

    Adding pin configuration

--- arduino/RFVelo/src/velo.ino
+++ arduino/RFVelo/src/velo.ino
@@ -4,6 +4,43 @@
 #include <avr/sleep.h>
 #include <avr/power.h>
 
+/************************/
+/** PINs configuration **/
+/************************/
+
+/* Button */
+const int BUTTON_PIN1 = 14; //A0
+const int BUTTON_PIN2 = 15; //A1
+
+/* LED */
+const int RED_PIN = 5;
+const int GREEN_PIN = 6;
+const int BLUE_PIN = 7;
+
+/* RF
+black module config: http://www.seeedstudio.com/document/pics/Interface.jpg
+1 - GND       |8 7 |
+2 - VCC       |6 5 |
+3 - CE        |4 3 |
+4 - CSN       |2[1]|
+5 - SCK
+6 - MOSI
+7 - MISO
+8 - IRQ */
+
+/* pour RF24 radio(9,10) as in RFCore (http://tmrh20.github.io/RF24/ (avec 7 -> 9 et 8 -> 10))
+GND -> GND
+VCC -> 3V3
+CE  -> 9
+CSN ->10
+MOSI ->11
+MISO ->12
+SCK ->13
+IRQ -> 2
+*/
+
+/* RFID */
+
 bool terminal_in_range = false;
 RFCore * rf_core;
 
@@ -13,8 +50,6 @@ void setup(void)
   printf_begin();
   int BIKE_ID = 8;//will read EEPROM after
   rf_core = new RFCore(BIKE_ID, false);
-
-
 }
 
 void loop(){
